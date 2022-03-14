99c88087a6ef5669a4a1d242bb098854655cb3c9
chae <king@chaeui-MacBookPro.local>
2022-03-14 15:33:46 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,27 @@
+// index.js
+const express = require('express'); // express 임포트
+const app = express(); // app생성
+const port = 5000;
+
+app.get('/', function (req, res) {
+  res.send('hello world!!');
+});
+
+app.listen(port, () => console.log(`${port}포트입니다.`));
+
+// 몽구스 연결
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://Ahyun:hoho0301@yarm.uxukv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      // useNewUrlPaser: true,
+      // useUnifiedTofology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB conected'))
+  .catch((err) => {
+    console.log(err);
+  });
